2d0a4c3158c44bac403b82cd3a02e5caf424632c
galaxies99 <galaxies@vip.qq.com>
2019-11-03 00:59:03 +0800

    update question 2

--- GM_gen.m
+++ GM_gen.m
@@ -1,32 +1,32 @@
 close all; clear all; clc
 
 % generate X from normal distribution
-mu1 = input('Please input mu1: ')
-sigma1 = input('Please input sigma1: ')
-X = normrnd(mu1, sigma1, [10000 1])
+mu1 = input('Please input mu1: ');
+sigma1 = input('Please input sigma1: ');
+X = normrnd(mu1, sigma1, [10000 1]);
 
 % generate Y from normal distribution
-mu2 = input('Please input mu2: ')
-sigma2 = input('Please input sigma2: ')
-Y = normrnd(mu2, sigma2, [10000 1])
+mu2 = input('Please input mu2: ');
+sigma2 = input('Please input sigma2: ');
+Y = normrnd(mu2, sigma2, [10000 1]);
 
 % generate eta
-p = input('Please input p: ')
-eta_prime = unifrnd(0, 1, [10000 1])
-eta = zeros(10000, 1)
-eta(eta_prime <= p) = 1
+p = input('Please input p: ');
+eta_prime = unifrnd(0, 1, [10000 1]);
+eta = zeros(10000, 1);
+eta(eta_prime <= p) = 1;
 
 % generate Z = X + eta * Y
-res = X + eta .* Y
+Z = X + eta .* Y;
 
 % graphing
-[counts, centers] = hist(res, 70);
+[counts, centers] = hist(Z, 70);
 figure
 bar(centers, counts / sum(counts))
 
 % printing data to file
-fp = fopen('gaussian_mixture_data.csv','w');
-for i = 1:10000
-    fprintf(fp,'%d,\n',res(i, 1));
+fp = fopen('gaussian_mixture_data.csv', 'w');
+for i = 1 : 10000
+    fprintf(fp, '%d,\n', Z(i, 1));
 end
-fclose(fp)
+fclose(fp);
